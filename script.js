e17108e10239ab81c1158805b719700e393ec86d
huz3y <huzeyhassan6@gmail.com>
2024-05-10 01:17:52 +0530

    add rotation logic

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const btn = document.querySelector(".btn");
+const activeBtn = document.querySelector(".btn--active");
+const cardFront = document.querySelector(".card__side--front");
+const cardBack = document.querySelector(".card__side--back");
+const ratingNum = document.querySelectorAll(".card__rating-num");
+
+//CARD ROTATION
+ratingNum.forEach((rating) => {
+  rating.addEventListener("click", () => {
+    btn.classList.add("btn--active");
+  });
+});
+
+btn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn--active")) {
+    cardFront.style.transform = "rotateY(-180deg)";
+    cardBack.style.transform = "rotateY(0)";
+  }
+});
